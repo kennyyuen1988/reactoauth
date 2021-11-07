4b4f3323717bacf8b8e0cec5020c8b37b7835465
Kenny Yuen <kennyyuen1988@gmail.com>
2021-11-07 15:22:35 +0800

    got email

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Auth } from 'aws-amplify'
+
 function App() {
+  async function checkUser(){
+    const user = await Auth.currentAuthenticatedUser()
+    console.log('user:', user);
+    console.log('email:', user.attributes.email);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button 
+          onClick={() => Auth.federatedSignIn({ provider:"Google" })} 
+        >Sign in with Google</button>
+
+        <button 
+          onClick={checkUser} 
+        >Check User</button>
+
+        <button 
+          onClick={() => Auth.signOut()}
+        >Sign Out</button>
       </header>
     </div>
   );
