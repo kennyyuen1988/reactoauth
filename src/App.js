e1af1fda5ea314b6e2de96cef93435c72cfa5fd1
Kenny Yuen <kennyyuen1988@gmail.com>
2021-11-07 21:12:10 +0800

    login with google and upload to s3 success

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
+import Amplify from '@aws-amplify/core';
+import { Auth, Storage } from 'aws-amplify';
 import './App.css';
 
-import { Auth } from 'aws-amplify'
-
 function App() {
-  async function checkUser(){
-    const user = await Auth.currentAuthenticatedUser()
-    console.log('user:', user);
-    console.log('email:', user.attributes.email);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+
+    Amplify.configure({
+        // Auth: {
+        //     identityPoolId: 'ca-central-1:1e2b9164-0bf8-483b-9819-6d3c210ba1ef', //REQUIRED - Amazon Cognito Identity Pool ID
+        //     region: 'ca-central-1', // REQUIRED - Amazon Cognito Region
+        //     //userPoolId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito User Pool ID
+        //     //userPoolWebClientId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito Web Client ID
+        // },
+        Storage: {
+            AWSS3: {
+                bucket: 'kenny-react-bucket-ca', //REQUIRED -  Amazon S3 bucket name
+                region: 'ca-central-1', //OPTIONAL -  Amazon service region
+            }
+        }
+    });
+
+  }, [])
+
+  useEffect(() => {
+
+    checkUser();
+
+  }, [])
+
+  async function checkUser() {
+    try {
+      const currentUser = await Auth.currentUserPoolUser();
+      setUser({username: currentUser.username, ...currentUser.attributes})
+      console.log(currentUser);     
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button 
-          onClick={() => Auth.federatedSignIn({ provider:"Google" })} 
-        >Sign in with Google</button>
 
-        <button 
-          onClick={checkUser} 
-        >Check User</button>
+  function logOff() {
+    Auth.signOut();
+  }
 
-        <button 
-          onClick={() => Auth.signOut()}
-        >Sign Out</button>
-      </header>
-    </div>
-  );
+  const ref = useRef(null);
+
+  const handleFileLoad = () => {
+    const filename = ref.current.files[0].name;
+    console.log(filename);
+    Storage.put(filename, ref.current.files[0]).then(resp => {
+      console.log(resp);
+    }).catch(err => {console.log(err);});
+  }
+
+  if(user) {
+    return(
+      <>
+        <p>{ user.username } : { user.email }</p>
+        <input ref={ref} type="file" onChange={handleFileLoad} />
+        <button onClick = { logOff} > Log off</button>
+      </>
+    )
+  } else {
+    return (
+      <button onClick={() => Auth.federatedSignIn( {provider: "Google"})}>Login via Google</button>
+    );
+  } 
 }
+
 
 export default App;
