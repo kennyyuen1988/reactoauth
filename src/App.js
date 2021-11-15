13691f8e5687cf2c84c19285fb255d706eef8f21
kennyyuenhk01 <kennyyuen@hk01.com>
2021-11-15 17:50:25 +0800

    login page updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import Amplify from '@aws-amplify/core';
 import { Auth, Storage } from 'aws-amplify';
 import './App.css';
 
+
 function App() {
+
   const [user, setUser] = useState(null);
 
   useEffect(() => {
 
     Amplify.configure({
-        // Auth: {
-        //     identityPoolId: 'ca-central-1:1e2b9164-0bf8-483b-9819-6d3c210ba1ef', //REQUIRED - Amazon Cognito Identity Pool ID
-        //     region: 'ca-central-1', // REQUIRED - Amazon Cognito Region
-        //     //userPoolId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito User Pool ID
-        //     //userPoolWebClientId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito Web Client ID
-        // },
         Storage: {
             AWSS3: {
                 bucket: 'kenny-react-bucket-ca', //REQUIRED -  Amazon S3 bucket name
@@ -62,16 +58,23 @@ function App() {
         <div className="header">
           <div className="menu-circle"></div>
           <div className="header-menu">
-            <a className="menu-link is-active" href="#">Upload Files</a>
-            <a className="menu-link notify" href="#">Your History</a>
-            <a className="menu-link" href="#">Remarks</a>
+            <button href="#" className="menu-link is-active">Upload Files</button>
+            <button href="#" className="menu-link notify">Your History</button>
+            <button href="#" className="menu-link">Remarks</button>
           </div>
           <div className="search-bar">
             <input type="text" placeholder="Search" />
           </div>
           <div className="header-profile">
             <h6>Welcome! { user.email }</h6>
-            <img className="profile-img" src="https://images.unsplash.com/photo-1600353068440-6361ef3a86e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" alt="" />
+            <div className="dropdown profilePic">
+              <ul>
+                <li><a href="#">My Profile</a></li>
+                <li><a href="#">Setting</a></li>
+                <li><a href="#">Logout</a></li>
+              </ul>
+              <img className="profile-img" src="https://images.unsplash.com/photo-1600353068440-6361ef3a86e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" alt="" />
+            </div>
           </div>
         </div>
         <p>Please upload your video:</p>
@@ -85,10 +88,10 @@ function App() {
         <div className="header">
           <div className="menu-circle"></div>
           <div className="header-menu">
-            <a className="menu-link is-active" href="#">Login</a>
+            <button href="#" className="menu-link is-active">Login</button>
           </div>
         </div>
-        <div className="desc">Please Login your google account</div>
+        <div className="desc">Please Login your google account!</div>
         <button id="loginViaGoogle" onClick={() => Auth.federatedSignIn( {provider: "Google"})}>
           <div className="google-btn">
             <div className="google-icon-wrapper">
